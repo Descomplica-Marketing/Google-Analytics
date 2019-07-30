f382ef6c2ba753251791231489db960d0c10ede2
Gustavo Tronchin <marketing-dev@descomplica.com.br>
2019-07-29 23:35:13 -0300

    Removing view ids

--- analytics_run.py
+++ analytics_run.py
@@ -1,6 +1,8 @@
 from functions import get_report_df
 
-view_ids = [('196069492', 'Todos os dados')]
+# Insert an array of your view ids in this format: [(view_id_1, view_id_1_name), (view_id_2, view_id_2_name)]
+
+view_ids = []
 
 df = get_report_df(view_ids=view_ids, metrics=[{'expression': 'ga:sessions'}, ],
                    dimensions=[{'name': 'ga:date'}, {'name': 'ga:source'},
